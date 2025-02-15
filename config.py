b2f1644843e3d23e795ef04ef1896fea6006b633
Kokopw <kmelon175@gmail.com>
2025-02-15 20:20:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config:
     API_HASH = environ.get("API_HASH", "e387e32ca3b2474492dc698928c4e501")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://romose6135:w4pP6opyA9ZdXHYV@cluster0.kn68k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://tapaje6625:9SWZXLOHS7d402Yj@cluster0.j11y4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "charliebots")
     BOT_OWNER = int(environ.get("BOT_OWNER", "1605422146"))
 
 # Don't Remove Credit Tg - @VJ_Botz
